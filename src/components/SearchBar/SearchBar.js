186a85f8745b5a82d7dca571bf30eb8ca7414872
Wesley <wesleyadwebster@gmail.com>
2022-03-15 01:24:40 -0500

    Page layout materializing, searchbar component made

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from './SearchBar.module.css';
+
+const { formContainer, labelAlign } = classes;
+
+const SearchBar = ({ }) => {
+    return (
+        <form className={formContainer}>
+            <input placeholder="Search article by words" />
+        </form>
+
+    );
+};
+
+
+export default SearchBar;
